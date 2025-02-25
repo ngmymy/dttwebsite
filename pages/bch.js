d6cb57d637dc72e1f5a542eda48028f26c416323
My My Nguyen <mymy49447@gmail.com>
2025-02-25 17:51:48 -0600

    worked on bch page

--- pages/bch.js
+++ pages/bch.js
@@ -34,25 +34,73 @@ export default function BCH() {
       </header>
 
       <main className={styles.main}>
+        <h1 className={styles.bchPageHeader}>Ban Chấp Hành 2024-2026</h1>
         <section className={styles.bchContainer}>
           <div className={styles.bchGrid}>
             <div className={styles.bchMember}>
-              <img src="/images/member1.jpg" alt="President" />
+              <img src="/doanlogo.png" alt="President" />
               <h3>Đoàn Trưởng</h3>
               <p className={styles.bchTitle}>Chapter President</p>
               <p>Teresa - Binh Xuan Nguyen</p>
             </div>
             <div className={styles.bchMember}>
-              <img src="/images/member2.jpg" alt="Vice President Admin" />
+              <img src="/doanlogo.png" alt="Vice President Admin" />
               <h3>Phó Quản Trị</h3>
               <p className={styles.bchTitle}>Chapter Vice President Of Administrative Affairs</p>
               <p>Isave - My My Trieu Nguyen</p>
             </div>
             <div className={styles.bchMember}>
-              <img src="/images/member3.jpg" alt="Vice President Academic" />
+              <img src="/doanlogo.png" alt="Vice President Academic" />
               <h3>Phó Nghiên Huấn</h3>
               <p className={styles.bchTitle}>Chapter Vice President Of Academic Affairs</p>
               <p>Dominico - Tyler Pham</p>
+            </div>
+          </div>
+        </section>
+        {/* secretary and treasurer */}
+        <section className={styles.bchContainer}>
+          <div className={styles.bchGrid}>
+            <div className={styles.bchMember}>
+              <img src="/doanlogo.png" alt="President" />
+              <h3>Thư Ký</h3>
+              <p className={styles.bchTitle}>Chapter Secretary</p>
+              <p>Maria Teresa - Nancy Chu</p>
+            </div>
+            <div className={styles.bchMember}>
+              <img src="/doanlogo.png" alt="Vice President Admin" />
+              <h3>Thủ Quỹ</h3>
+              <p className={styles.bchTitle}>Chapter Treasurer</p>
+              <p>Anna - Evelyn Nhu Nguyen</p>
+            </div>
+          </div>
+        </section>
+        {/* nganh truongs */}
+        <hr></hr>
+        <section className={styles.bchContainer}>
+          <div className={styles.bchGrid}>
+            <div className={styles.bchMember}>
+              <img src="/doanlogo.png" alt="President" />
+              <h3>Ngành Trưởng Ấu Nhi</h3>
+              <p className={styles.bchTitle}>Division Leader Seedling Specialist</p>
+              <p className={styles.bchName}>Teresa - Binh Xuan Nguyen</p>
+            </div>
+            <div className={styles.bchMember}>
+              <img src="/doanlogo.png" alt="Vice President Admin" />
+              <h3>Ngành Trưởng Thiếu Nhi</h3>
+              <p className={styles.bchTitle}>Division Leader Search Specialist</p>
+              <p>Dominico - Tyler Pham</p>
+            </div>
+            <div className={styles.bchMember}>
+              <img src="/doanlogo.png" alt="Vice President Academic" />
+              <h3>Ngành Trưởng Nghĩa Sĩ</h3>
+              <p className={styles.bchTitle}>Division Leader Companion Specialist</p>
+              <p>Maria Teresa - Nancy Chu</p>
+            </div>
+            <div className={styles.bchMember}>
+              <img src="/doanlogo.png" alt="Vice President Academic" />
+              <h3>Ngành Trưởng Hiệp Sĩ</h3>
+              <p className={styles.bchTitle}>Division Leader KoE Specialist</p>
+              <p>Maria - Sarah Le Pham</p>
             </div>
           </div>
         </section>
