211fe67b6644860f8068ac1af2829f74512315d1
My My Nguyen <mymy49447@gmail.com>
2025-06-24 21:41:38 -0500

    fixed contact message box

--- pages/footer.js
+++ pages/footer.js
@@ -2,7 +2,7 @@ export default function footer() {
     return (
         <footer>
             <a href="https://github.com/ngmymy" target="_blank" rel="noopener noreferrer">
-                Maintained by Đoàn Tôma Thiện, MN. Last updated on 3/18/2025
+                Maintained by Đoàn Tôma Thiện, MN. Last updated on 6/24/2025
             </a>
         </footer>
     )
