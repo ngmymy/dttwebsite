50e8370288582d3cd75a532c076a0ab3de007828
My My Nguyen <mymy49447@gmail.com>
2025-08-02 05:57:05 -0500

    debugging deployment

--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    optimizeFonts: false, // Disable Next.js font optimization to prevent conflicts
-  }
-  
-  module.exports = nextConfig
